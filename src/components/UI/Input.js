8867cd7a8c9ad1b0719a4e66ffd4eb25bbcf5c45
BakiZy <milosh88@live.com>
2022-04-30 10:25:20 +0200

    ui

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,11 +1,12 @@
 import classes from "./Input.module.css";
-
-const Input = (props) => {
+import React from "react";
+//wrapping component with forwardRef to get access to ref and use it as a parameter in the function
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
-};
+});
 export default Input;
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,4 +1,32 @@
 import classes from "./Modal.module.css";
-const Modal = (props) => {};
+import React, { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const BackDrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onClose} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onClose={props.onClose} />,
+        document.getElementById("overlays")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        document.getElementById("overlays")
+      )}
+    </Fragment>
+  );
+};
 
 export default Modal;
